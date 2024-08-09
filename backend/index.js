babaeb7137185f0a40018fcf8cae58d3bd6e66a1
ravi <rkravisingh890@gmail.com>
2024-08-09 12:10:41 +0530

    root route

--- backend/index.js
+++ backend/index.js
@@ -4,12 +4,16 @@ const cors=require('cors')
 
 connectToMongo();
 const app =express()
-const port = 5000;
+const port = 5500;
 
 app.use(cors())
 app.use(express.json())
 
 //Available Routes
+app.get("/", (req, res, next) => {
+    res.set('Content-Type', 'text/html');
+    res.status(200).send('<h1 style="margin-left: 20px; margin-top: 20px;">Welcome to iNotebook.</h1>');
+});
 app.use('/api/auth',require('./routes/auth'))
 app.use('/api/notes',require('./routes/notes'))
 
